ed4cdad0789f9d7d07e7d0bfbd70e4df31aaac4f
Dana Simmons <dana@simmons-audio.com>
2019-04-04 15:37:33 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    state = {
+        message: "",
+        number: null,
+        people: []
+    }
+    componentDidMount() {
+      fetch('http://api.open-notify.org/astros.json')
+      .then(resp => resp.json())
+      .then(data=>this.setState(data))
+    }
+    
+    
+  render() {
+    return (
+    <div>
+        <p>Number: {this.state.number}</p>
+        <ul>
+            {this.state.people.map((p)=> <li>Craft: {p.craft} Name: {p.name}</li>)}
+        </ul>
+    </div>
+    )
+  }
+}
